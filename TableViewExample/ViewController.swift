9ab8f0b65f028e0caa7268efb1fa82ea6c0c67d3
Sonar <Sonar@imacs-imac.local>
2018-12-13 20:20:04 -0500

    Initial Commit

---
+++ TableViewExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewExample
+//
+//  Created by Sonar on 12/13/18.
+//  Copyright © 2018 Sonar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TableViewExampleTests/TableViewExampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  TableViewExampleTests.swift
+//  TableViewExampleTests
+//
+//  Created by Sonar on 12/13/18.
+//  Copyright © 2018 Sonar. All rights reserved.
+//
+
+import XCTest
+@testable import TableViewExample
+
+class TableViewExampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
